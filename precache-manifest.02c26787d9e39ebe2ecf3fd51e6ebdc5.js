92e9127028460e958d72aac8585a638f10bead64
agungjati <agungjati94@gmail.com>
2019-06-30 23:44:08 +0700

    ok

---
+++ precache-manifest.02c26787d9e39ebe2ecf3fd51e6ebdc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda467a9e2fe4fd809a5c32cf4da4380",
+    "url": "/perpus-ui-react/index.html"
+  },
+  {
+    "revision": "e0b5011037c4c4a494d8",
+    "url": "/perpus-ui-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff40a91a0bb0144f63e4",
+    "url": "/perpus-ui-react/static/js/2.3d4ff719.chunk.js"
+  },
+  {
+    "revision": "e0b5011037c4c4a494d8",
+    "url": "/perpus-ui-react/static/js/main.b0dcfc8e.chunk.js"
+  },
+  {
+    "revision": "174c013d75a8848098dc",
+    "url": "/perpus-ui-react/static/js/runtime~main.78846016.js"
+  }
+]);
